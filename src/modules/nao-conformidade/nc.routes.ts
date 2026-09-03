5698c1e71fb8c303ce1210a4d58f11d3b5def3da
Mateus Felipe Mendes <fellipemendes1@gmail.com>
2026-09-03 16:27:58 -0300

    feat: adiciona tratamento de erros com status HTTP apropriados

--- src/modules/nao-conformidade/nc.routes.ts
+++ src/modules/nao-conformidade/nc.routes.ts
@@ -3,6 +3,6 @@ import { ncController } from "./nc.controller.js";
 
 export async function ncRoutes(app: FastifyInstance) {
     app.get("/nao-conformidades", ncController.listar);
-    app.get("/nao-conformidades/:id", ncController.buscarPorId)
-    app.post("/nao-conformidades", ncController.criar)
+    app.get("/nao-conformidades/:id", ncController.buscarPorId);
+    app.post("/nao-conformidades", ncController.criar);
 }
